e26c44e25f972153503d08bcb63fa0404220de77
SnehaMudavath <smudavath@myseneca.ca>
2022-04-14 05:20:37 +0000

    Added .gitignore and made few changes in userdata

--- modules/asg/install_httpd.sh
+++ modules/asg/install_httpd.sh
@@ -2,6 +2,6 @@
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>Welcome to our ${prefix} in ${env}! Group 23 members: Sneha Pillay, Sneha Mudavat, Arun and Varnika| Request resolved by $(hostname -f) | Built by Terraform!"  >  /var/www/html/index.html
+echo "<h1>Welcome to our ${prefix} in ${env}! Group 24 members: Sneha Pillay, Sneha Mudavath, Arun Bains and Varnika Bassi| Request resolved by $(hostname -f) | Built by Terraform!"  >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
